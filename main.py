17ac32d245462b2f10a1346d8d38e6adfe76b7a3
MrWlobo <michaldworniczak40@gmail.com>
2026-04-04 22:07:20 +0200

    Added basic model for text to logic parsing.

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
 load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
 
-api_key = os.getenv("OPENAI_API_KEY")
-print(api_key)
+model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash-lite",
+    google_api_key=api_key,
+    temperature=0
+)
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", """You are a semantic parser.
+        Convert the sentence into FIRST-ORDER LOGIC.
+        Rules:
+        - Use standard logical notation
+        - Output ONLY the formula
+        - No explanation
+        - No extra text"""),
+    ("human", "{input_sentence}")
+])
+
+chain = prompt | model | StrOutputParser()
+
+sentence = "Every human is mortal.."
+result = chain.invoke({"input_sentence": sentence})
+
+print(f"Sentence: {sentence}")
+print(f"Logic: {result}")
