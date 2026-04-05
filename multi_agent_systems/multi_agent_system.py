fc74473f7dc80e44efc978275da622002895e1f7
MrWlobo <michaldworniczak40@gmail.com>
2026-04-05 18:48:12 +0200

    Added parent class MAS and a basic implementation of MAS that involves generator, critic and solver

---
+++ multi_agent_systems/multi_agent_system.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+
+
+class MultiAgentSystem(ABC):
+    def __init__(self, max_iterations: int):
+        self.max_iterations = max_iterations
+
+    @abstractmethod
+    def run(self, sentence: str):
+        pass
---
+++ multi_agent_systems/simple_multi_agent_system.py
@@ -0,0 +1,52 @@
+from agents.critic_agent import CriticAgent
+from agents.generator_agent import GeneratorAgent
+from multi_agent_systems.multi_agent_system import MultiAgentSystem
+import logging
+
+class SimpleMultiAgentSystem(MultiAgentSystem):
+    def __init__(self, max_iterations: int,
+                 generator_agent: GeneratorAgent,
+                 critic_agent: CriticAgent,
+                 solver,
+                 verbose=True):
+        super().__init__(max_iterations)
+        self.generator_agent = generator_agent
+        self.critic_agent = critic_agent
+        self.solver = solver
+        self.verbose = verbose
+
+    def run(self, sentence: str):
+        original_sentence = sentence
+        generator_input = sentence
+        for i in range(self.max_iterations):
+            generator_output = self.generator_agent.create_prompt(generator_input)
+            if self.verbose:
+                logging.info(f"Generator output in iteration {i + 1}: {generator_output}")
+
+            critic_output = self.critic_agent.verify_logic(original_sentence, generator_output)
+            if self.verbose:
+                logging.info(f"Critic output in iteration {i + 1}: {critic_output}")
+
+            if critic_output["status"] == "ERROR":
+                generator_input = (
+                    f"Your previous attempt for the sentence: '{original_sentence}' was incorrect. "
+                    f"Critic's feedback: {critic_output['feedback']}. "
+                    f"Please try again and return the corrected JSON."
+                )
+                continue
+
+            premises = generator_output["premises"]
+            goal = generator_output["goal"]
+
+            self.solver.set_premises(premises)
+            self.solver.set_goal(goal)
+            result = self.solver.prove_goal()
+            if self.verbose:
+                logging.info(f"MAS finished the task after {i + 1} iterations, and determined the following result: {result}")
+
+            return result
+
+        if self.verbose:
+            logging.info(f"Maximum number of iterations ({self.max_iterations}) exceeded, MAS reached no conclusions.")
+
+        return None
